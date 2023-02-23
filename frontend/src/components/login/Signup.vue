7fa0a7fd3a3eff48377143e4c37aedf4d9b82cc1
kev <ks1202210@gmail.com>
2023-02-23 11:13:24 +0100

    fixed signup email sent message

--- frontend/src/components/login/Signup.vue
+++ frontend/src/components/login/Signup.vue
@@ -23,7 +23,7 @@ function signedup(){
                 Vielen Dank für Ihre Anmeldung.
             </div>
             <div>
-                Zum Regestrierung abzuschließen klicken Sie bitte auf den Link in der E-Mail, die wir Ihnen gesendet haben.
+                Zum Abschließen Ihrer Regestrierung folgen Sie bitte dem Link in der E-Mail, die wir Ihnen gesendet haben.
             </div>
             <div>
                 Zum login <a @click='$emit("login")'>hier</a> klicken.
