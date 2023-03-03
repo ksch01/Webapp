999e65569c73b8e614d6ec62f6d07054b1834475
kev <ks1202210@gmail.com>
2023-03-03 09:28:27 +0100

    added symfony api delete user endpoint

--- symfony_backend/src/Controller/UserController.php
+++ symfony_backend/src/Controller/UserController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use App\Entity\User;
@@ -24,12 +28,44 @@ class UserController extends AbstractController{
         $place = $request->query->get('place', '');
         $phone = $request->query->get('phone', '');
 
-        $users = $repository->searchAllBy(["email" => $email, "name" => $name, "zip" => $zip, "place" => $place, "phone" => $phone]);
+        $users = $repository->searchAllBy([
+            "email" => $email, 
+            "name" => $name, 
+            "zip" => $zip, 
+            "place" => $place, 
+            "phone" => $phone]);
+
         foreach($users as $key=>$value){
             $users[$key] = new UserPrep($value);
         }
-
         return $this->json($users);
+    }
+
+    #[Route('/user', name:'api_user_delete', methods:['DELETE'])]
+    public function deleteUser(UserRepository $repository, Request $request) : Response{
+
+        $sessionKey = $request->query->get('session');
+        $targetemail = $request->query->get('target');
+    
+        if($sessionKey === null || $targetemail === null){
+            throw new BadRequestHttpException('delete user requests require the session and target query parameter to be set');
+        }
+
+        $invoker = $repository->findOneBy(["session" => $sessionKey]);
+        if(!$invoker || !$invoker->getUsergroup()->isPrivDelete()){
+            throw new HttpException(403, 'not enough privileges');
+        }
+
+        $target = $repository->find($targetemail);
+        if(!$target){
+            throw new NotFoundHttpException('delete user target "' . $targetemail . '" could not be found');
+        }
+
+        $repository->remove($target, true);
+
+        $response = new Response();
+        $response->setStatusCode(200);
+        return $response;
     }
 }
 
